19ef6142b9068ef0564c86d739cf65e5d89a4b98
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2021-06-27 09:32:23 -0300

    feat(App.tsx): register routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/Auth'
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+import { Room } from './pages/Room'
+import { AdminRoom } from './pages/AdminRoom'
+
+import { GlobalStyles } from './assets/styles/global'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:room_id" component={Room} />
+          <Route path="/admin/rooms/:room_id" component={AdminRoom} />
+        </Switch>
+      </BrowserRouter>
+      <GlobalStyles />
+    </AuthProvider>
+  )
 }
 
 export default App;
